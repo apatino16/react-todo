f2c2d2d63b6ece7d5b91db19237c4a8ed5ca4909
Anny Patino <a.a.patino16@gmail.com>
2023-03-21 01:58:02 -0700

    Created a form and refactored App component to render the Add Todo Form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import TodoList from "./TodoList";
+import AddTodoForm from "./AddTodoForm";
 
 function App() {
   return (
     <div>
       <h1 style={{ textAlign: "center" }}> Todo List </h1>
+      <AddTodoForm />
       <TodoList />
     </div>
   );
