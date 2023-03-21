69eb52781badc4260b6cc8cda7a065a2b7716c93
Anny Patino <a.a.patino16@gmail.com>
2023-03-21 01:45:21 -0700

    Refactored the App component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from "react";
-
-const todoList = [
-  { objectID: 0, title: "Prepare lunch for work." },
-  { objectID: 1, title: "Submit CTD homework." },
-  { objectID: 2, title: "Read next week's lesson." },
-];
+import TodoList from "./TodoList";
 
 function App() {
   return (
     <div>
       <h1 style={{ textAlign: "center" }}> Todo List </h1>
-      <ul>
-        {todoList.map(function (item) {
-          return <li key={item.objectID}>{item.title}</li>;
-        })}
-      </ul>
+      <TodoList />
     </div>
   );
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,19 @@
 import React from "react";
 
-function TodoList() {}
+const todoList = [
+  { objectID: 0, title: "Prepare lunch for work." },
+  { objectID: 1, title: "Submit CTD homework." },
+  { objectID: 2, title: "Read next week's lesson." },
+];
+
+function TodoList() {
+  return (
+    <ul>
+      {todoList.map(function (item) {
+        return <li key={item.objectID}>{item.title}</li>;
+      })}
+    </ul>
+  );
+}
 
 export default TodoList;
