633745a66de778beefaba43f408adb84aa85d7f7
Anny Patino <a.a.patino16@gmail.com>
2023-06-03 14:56:53 -0700

    removinbg todo list item feature

--- src/App.js
+++ src/App.js
@@ -36,11 +36,26 @@ const App = () => {
   useEffect(() => {
     fetchData();
   }, []);
+  const removeTodo = async (id) => {
+    const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}/${id}`;
+    const options = {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
+        "Content-type": "application/json",
+      },
+    };
 
-  const removeTodo = (id) => {
-    const newArray = todoList.filter((todo) => todo.id !== id);
-
-    setTodoList(newArray);
+    try {
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      const newTodoList = todoList.filter((todo) => todo.id !== id);
+      setTodoList(newTodoList);
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
   const addTodo = async (title) => {
